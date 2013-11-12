49f9fdb5f3677e28c319e64e602ce020776f55e8
Greg Kepler <gkepler@gmail.com>
2013-11-11 21:13:49 -0500

    Added working box2d 5-1

--- Cinder/chp5_physicslibraries/box2d/NOC_5_01_box2d_exercise/src/NOC_5_01_box2d_exerciseApp.cpp
+++ Cinder/chp5_physicslibraries/box2d/NOC_5_01_box2d_exercise/src/NOC_5_01_box2d_exerciseApp.cpp
@@ -1,7 +1,16 @@
+//
+//  Example 5-1: Box2d Exercise
+//  The Nature of Code
+//
+//  Converted from Daniel Shiffman's Processing Examples
+//  Created by Greg Kepler
+//
+
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
 
 #include <Box2D/Box2D.h>
+#include "Box.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -10,89 +19,77 @@ using namespace std;
 const float BOX_SIZE = 10;
 
 class NOC_5_01_box2d_exerciseApp : public AppNative {
-  public:
+public:
+    void prepareSettings( Settings *settings );
 	void setup();
-	void mouseDown( MouseEvent event );	
+	void mouseDown( MouseEvent event );
+    void mouseUp( MouseEvent event );
+    void mouseDrag( MouseEvent event );
 	void update();
 	void draw();
 	
-	void addBox( const Vec2f &pos );
-	
 	b2World				*mWorld;
-	vector<b2Body*>		mBoxes;
+	vector<Box*>		mBoxes;
+    bool                mousePressed;
+    Vec2f               mousePos;
 };
+
+void NOC_5_01_box2d_exerciseApp::prepareSettings( Settings *settings )
+{
+    settings->setWindowSize( 640, 360 );
+}
 
 void NOC_5_01_box2d_exerciseApp::setup()
 {
+    // In cinder, you have to set the gravity of your world in initialization
 	b2Vec2 gravity( 0.0f, 10.0f );
 	mWorld = new b2World( gravity );
-
-	b2BodyDef groundBodyDef;
-	groundBodyDef.position.Set( 0.0f, getWindowHeight() );
-	b2Body* groundBody = mWorld->CreateBody(&groundBodyDef);
-
-	// Define the ground box shape.
-	b2PolygonShape groundBox;
-
-	// The extents are the half-widths of the box.
-	groundBox.SetAsBox( getWindowWidth(), 10.0f );
-
-	// Add the ground fixture to the ground body.
-	groundBody->CreateFixture(&groundBox, 0.0f);
-	
-}
-
-void NOC_5_01_box2d_exerciseApp::addBox( const Vec2f &pos )
-{
-	b2BodyDef bodyDef;
-	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set( pos.x, pos.y );
-
-	b2Body *body = mWorld->CreateBody( &bodyDef );
-
-	b2PolygonShape dynamicBox;
-	dynamicBox.SetAsBox( BOX_SIZE, BOX_SIZE );
-
-	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicBox;
-	fixtureDef.density = 1.0f;
-	fixtureDef.friction = 0.3f;
-	fixtureDef.restitution = 0.5f; // bounce
-
-	body->CreateFixture( &fixtureDef );
-	mBoxes.push_back( body );
+    mousePressed = false;
 }
 
 void NOC_5_01_box2d_exerciseApp::mouseDown( MouseEvent event )
 {
-	addBox( event.getPos() );
+    mousePos = event.getPos();
+    mousePressed = true;
+}
+
+void NOC_5_01_box2d_exerciseApp::mouseUp( MouseEvent event )
+{
+    mousePressed = false;
+}
+
+void NOC_5_01_box2d_exerciseApp::mouseDrag( MouseEvent event )
+{
+    mousePos = event.getPos();
 }
 
 void NOC_5_01_box2d_exerciseApp::update()
 {
-	for( int i = 0; i < 10; ++i )
+	// The Box2d Step function is a bit more involved in Cinder
+    // We have to specify:
+    // - time step
+    // - velocity iterations
+    // - position iterations
+    
+    for( int i = 0; i < 5; ++i )
 		mWorld->Step( 1 / 30.0f, 10, 10 );
 }
 
 void NOC_5_01_box2d_exerciseApp::draw()
 {
-	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) );
+	gl::clear( Color::white() );
 	
-	gl::color( Color( 1, 0.5f, 0.25f ) );
-	for( vector<b2Body*>::const_iterator boxIt = mBoxes.begin(); boxIt != mBoxes.end(); ++boxIt ) {
-		Vec2f pos( (*boxIt)->GetPosition().x, (*boxIt)->GetPosition().y );
-		float t = toDegrees( (*boxIt)->GetAngle() );
-
-		glPushMatrix();
-		gl::translate( pos );
-		gl::rotate( t );
-
-		Rectf rect( -BOX_SIZE, -BOX_SIZE, BOX_SIZE, BOX_SIZE );
-		gl::drawSolidRect( rect );
-
-		glPopMatrix();	
-	}
+	// When the mouse is clicked, add a new Box object
+    if (mousePressed) {
+        Box *b = new Box( mWorld, mousePos );
+        mBoxes.push_back( b );
+    }
+    
+    // Display all the boxes
+    for( vector<Box*>::const_iterator boxIt = mBoxes.begin(); boxIt != mBoxes.end(); ++boxIt ) {
+        (*boxIt)->display();
+    }
 }
 
 CINDER_APP_NATIVE( NOC_5_01_box2d_exerciseApp, RendererGl )
+
