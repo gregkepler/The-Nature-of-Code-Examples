f599502ded15b41a8813f6b1cc07b95b0cd06ae1
Greg Kepler <gkepler@gmail.com>
2013-11-20 06:49:05 -0500

    Started box2d Ex5-03

---
+++ Cinder/chp5_physicslibraries/box2d/NOC_5_03_ChainShape_SimpleApp/src/Particle.cpp
@@ -0,0 +1,9 @@
+//
+//  Particle.cpp
+//  NOC_5_03_ChainShape_SimpleApp
+//
+//  Created by Greg Kepler on 11/15/13.
+//
+//
+
+#include "Particle.h"
---
+++ Cinder/chp5_physicslibraries/box2d/NOC_5_03_ChainShape_SimpleApp/src/Particle.h
@@ -0,0 +1,12 @@
+//
+//  Particle.h
+//  NOC_5_03_ChainShape_SimpleApp
+//
+//  Created by Greg Kepler
+//  A circular particle
+//
+
+class Particle{
+public:
+    Particle();
+}
